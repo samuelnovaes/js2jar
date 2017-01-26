db2c3c084490802c0fc1f4daebf2db8e37bdd2db
Samuel Novaes Sena <samuel.novaes.96@gmail.com>
2017-01-26 07:21:59 -0200

    26/01/2017 07:21

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const fs = require("fs");
 const path = require("path");
 const cp = require('child_process');
-var clc = require("cli-color");
 
 let file = process.argv[2];
 let loadRegex = /load *\( *"(.*)" *\)( *;)?/
@@ -27,20 +26,20 @@ if(file){
 		if(err) throw err;
 		let compileClass = cp.spawn("javac", [`${className}.java`]);
 		compileClass.stdout.on('data', data=>console.log(`stdout: ${data}`));
-		compileClass.stderr.on('data', data=>console.log(clc.redBright(`stderr: ${data}`)));
+		compileClass.stderr.on('data', data=>console.log(`stderr: ${data}`));
 		compileClass.on('close', (code) => {
 			if(code == 0){
 				if(err) throw err;
 				let compileJar = cp.spawn("jar", ["cvfe", `${className}.jar`, className, `${className}.class`]);
 				compileJar.stdout.on('data', data=>console.log(`stdout: ${data}`));
-				compileJar.stderr.on('data', data=>console.log(clc.redBright(`stderr: ${data}`)));
+				compileJar.stderr.on('data', data=>console.log(`stderr: ${data}`));
 				compileJar.on('close', (code) => {
 					if(code == 0){
 						fs.unlinkSync(`${className}.java`);
 						fs.unlinkSync(`${className}.class`);
 						let runJar = cp.spawn("java", ["-jar", `${className}.jar`]);
 						runJar.stdout.on('data', data=>console.log(`stdout: ${data}`));
-						runJar.stderr.on('data', data=>console.log(clc.redBright(`stderr: ${data}`)));
+						runJar.stderr.on('data', data=>console.log(`stderr: ${data}`));
 					}
 				});
 			}
